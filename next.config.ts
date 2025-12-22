c65f60c2de7e9f25670d847e92474749cfe692b0
Vladimir Babic <vldbbc@gmail.com>
2025-12-22 23:20:56 +0100

    Add webflow hostname for company icons

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'uploads-ssl.webflow.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'i.ytimg.com',
       },
       {
